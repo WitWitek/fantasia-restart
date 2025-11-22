7fe6d81b556e70c4e0c90fb5b8597c920e8aee7f
Wit Witek <49036621+WitWitek@users.noreply.github.com>
2025-11-22 19:38:39 +0100

    Update GraphicClasses.ts
    upgraded version with ai

--- fantasia restart/src/game/engine/GraphicClasses.ts
+++ fantasia restart/src/game/engine/GraphicClasses.ts
@@ -1,6 +1,7 @@
 
 
-
+import { seedParser } from "/seedParser";
+import type{WorkerRole} from "/Types";
 
 export const GFX: Record<string, HTMLImageElement> = {
   worker: loadImage("../../../graphics/worker.png"),
@@ -34,11 +35,15 @@ public graphics: HTMLImageElement | undefined;
 	}
 }
 export class Worker extends GraphicObject{
-
-	
-	constructor(x: number, y: number, w: number, h: number, src: string){
+		public data:any;
+		public target:string;
+		public role:WorkerRole;
+	constructor(x: number, y: number, w: number, h: number, src: string,seed:string,role:WorkerRole){
 		super(x,y,w,h,src);
-		
+		if(!seed)this.data=null;
+		this.data=seedParser(seed);
+		this.role=role;
+		this.target="hq";
 	}
 	moveToAnchor(x: number,y: number){
 		if(x>this.x)this.x++;
@@ -47,4 +52,90 @@ export class Worker extends GraphicObject{
 		else if(y<this.y)this.y--
 	
 	}
+	aiMovement(building:{name:string}){
+		const hq=this.data.objects.hq;
+		const stone=this.data.objects.stone;
+		const wood=this.data.objects.wood;
+		const fruits=this.data.objects.fruits;
+		const clay=this.data.objects.clay;
+		switch(this.target){
+			case "hq":
+			this.moveToAnchor(hq.x,hq.y);
+			if(this.x==hq.x&&this.y==hq.y){
+				switch(this.role){
+					case "woodcutter":
+					this.target="wood";
+					break;
+					case "miner":
+					this.target="stone";
+					break;
+					case "gatherer":
+					this.target="fruits";
+					break;
+					case"clayPicker":
+					this.target="clay";
+					break;
+				}
+			}
+			break;
+			case"wood":
+			this.moveToAnchor(wood.x,wood.y);
+			if(this.x==wood.x&&this.y==wood.y)this.target=building.name;
+			break;
+			case"stone":
+			this.moveToAnchor(stone.x,stone.y);
+			if(this.x==stone.x&&this.y==stone.y)this.target=building.name;
+			break;
+			case"fruits":
+			this.moveToAnchor(fruits.x,fruits.y);
+			if(this.x==fruits.x&&this.y==fruits.y)this.target=building.name;
+			break;
+			case"clay":
+			this.moveToAnchor(clay.x,clay.y);
+			if(this.x==clay.x&&this.y==clay.y)this.target=building.name;
+			break;
+			case"farm":
+			this.moveToAnchor(hq.x+30,hq.y);
+			if(this.x==hq.x+30&&this.y==hq.y){
+				this.target="hq"
+			}
+			break;
+			case"hunterHut":
+			this.moveToAnchor(hq.x+30,hq.y-20);
+			if(this.x==hq.x+30&&this.y==hq.y-20){
+				this.target="hq"
+			}
+			break;
+			case"carpentry":
+			this.moveToAnchor(hq.x+30,hq.y+20);
+			if(this.x==hq.x+30&&this.y==hq.y+20){
+				this.target="hq"
+			}
+			break;
+			case"masonry":
+			this.moveToAnchor(hq.x-30,hq.y);
+			if(this.x==hq.x-30&&this.y==hq.y){
+				this.target="hq"
+			}
+			break;
+			case"pottery":
+			this.moveToAnchor(hq.x-30,hq.y-20);
+			if(this.x==hq.x-30&&this.y==hq.y-20){
+				this.target="hq"
+			}
+			break;
+			case"house":
+			this.moveToAnchor(hq.x-30,hq.y+20);
+			if(this.x==hq.x-30&&this.y==hq.y+20){
+				this.target="hq"
+			}
+			break;
+		}
+		
+	}
+	multipleAi(speedPerTick, building) {
+  for (let i = 0; i < speedPerTick; i++) {
+    this.aiMovement(building);
+  }
+}
 }
